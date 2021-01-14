2824e486386a23ba25bb2a87ebc879c18a2ed514
jake <jacob.perman@gmail.com>
2021-01-14 15:43:32 -0600

    up to date

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -7,12 +7,36 @@ The picture is 500px wide and 400px tall. Look up ARC in the documentation to do
 '''
 import arcade
 
-arcade.open_window(500,400, "Jedi test")
+# draw window with width 500 & height 400
+arcade.open_window(500, 400, "Jedi test")
+arcade.set_background_color(arcade.color.ALMOND)
 
-for i in range(1,20):
-    x = 20
-    arcade.draw_line(x,0,x,400, arcade.color.BLACK, 2)
-    x += 20
+
+arcade.start_render()
+# draw vertical grid lines
 for i in range(1, 501, 20):
-    arcade.draw_line(i, 0, i, 500, arcade.color.BLACK, 2)
+    arcade.draw_line(i, 0, i, 400, arcade.color.BLACK, 2)
+# draw horizontal grid lines
+for i in range(1, 401, 20):
+    arcade.draw_line(0, i, 500, i, arcade.color.BLACK, 2)
+# draw rectangle at x=50, y=370, with width 60 & height 20
+arcade.draw_rectangle_filled(50, 370, 60, 20, arcade.color.PHLOX)
+# draw rectangle at x=200, y=260, with width 40 & height 20
+arcade.draw_rectangle_filled(200, 260, 40, 20, arcade.color.BLUSH, -45)
+# draw circle at (250, 200) with a radius of 40
+arcade.draw_circle_filled(250, 200, 40, arcade.color.WISTERIA)
+# Draw text at (20, 155) in 23pt size
+arcade.draw_text("I love you. I know.", 20, 155, arcade.color.BRICK_RED, 23)
+# draw blue line
+arcade.draw_line(80, 20, 120, 60, arcade.color.BLUE, 1)
+# draw the orange oval
+arcade.draw_ellipse_filled(100,100, 120, 40, arcade.color.AMBER)
+# draw the yellow pacman thing
+arcade.draw_arc_filled(400,320, 120, 120, arcade.color.YELLOW, 30, 330)
+# draw the small red square
+arcade.draw_rectangle_filled(461,10, 6, 6, arcade.color.RED)
+arcade.finish_render()
+
+
+arcade.run()
 
--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -9,3 +9,60 @@ I used a draw_text command and used 20 pt. asterisks for the stars.
 We will have a competition to see who can make this flag in the least lines of code.
 The record is 16! You will have to use some loops to achieve this.
 '''
+import arcade
+
+
+# function to draw a star
+def star(x, y):
+    draw = ((x-6, y+2), (x-1, y+1), (x, y+6), (x+2, y+1), (x+6, y+2), (x+2, y-1.5), (x+5, y-6), (x, y-3), (x-5, y-6),
+            (x-2, y-1.5))
+    arcade.draw_polygon_filled(draw, arcade.color.WHITE)
+
+
+# addressable database of flag proportions
+def num(x):
+    index = ord(x)-97
+    print(index)
+    values = [260, 494, 140, 198, 14, 14, 16.4, 16.4, 16, None, None, 20]
+    px = values[index]
+    print(px)
+    return px
+
+
+# open window to draw flag
+arcade.open_window(num('b'), num('a'), "Flag")
+arcade.set_background_color(arcade.color.WHITE)
+# start of render
+arcade.start_render()
+# draw the stripes
+for i in range(0, 300, 40):
+    arcade.draw_rectangle_filled(num('b')/2, 10+i, num('b'), num('l'), (191, 10, 48))
+# draw the flag outline
+arcade.draw_rectangle_outline(247, 130, num('b'), num('a'), arcade.color.BLACK, 5)
+# draw the blue section of flag (xpos, ypos, d, c)
+arcade.draw_rectangle_filled(99, 190, num('d'), num('c'), (0, 40, 104))
+
+
+# drawing of the stars
+times = 0
+# loops for each y-level stars should be drawn at
+for level in range(1, 10):
+    # sets current y position
+    ypos = (num('a') - (num('f')*times))-num('e')
+    times += 1
+    # changes star position and count based on whether an odd or even row
+    if level%2 == 0:
+        count = 5
+        xdis = num('h')*2
+    else:
+        count = 6
+        xdis = num('g')
+    # draws stars across flag at current y level
+    for stars in range(0, count):
+        # xpos = num('g'), num('d') - num('g'), num('h') * 2
+        # offsets the x position each time the loop runs
+        xpos = (num('g')*stars) * 2 + xdis
+        star(xpos, ypos)
+# end of render
+arcade.finish_render()
+arcade.run()
